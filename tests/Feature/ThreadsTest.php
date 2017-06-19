379ecf57a1970bd5c0ad911ebd324a3312d4fda7
mariorosenthal13 <mario.f.rosenthal@gmail.com>
2017-06-19 00:27:59 -0400

    refactored set up test

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -14,19 +14,31 @@ class ThreadsTest extends TestCase
      *
      * @return void
      */
+    public function setUp(){
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
     public function test_a_user_can_browse_threads()
     {
-        $thread = factory('App\Thread')->create();
+//        $thread = factory('App\Thread')->create();
 
         $response = $this->get('/threads');
-        $response->assertSee($thread->title);
+        $response->assertSee($this ->thread->title);
     }
 
     public function test_a_user_can_read_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
+//        $thread = factory('App\Thread')->create();
 
-        $response = $this->get('/threads/' . $thread->id);
-        $response->assertSee($thread->title);
+        $response = $this->get('/threads/' . $this->thread->id);
+        $response->assertSee($this->thread->title);
+    }
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        // given we have a thread
+        // and that thread includes replies
+        // when we visit a thread page
+        // then we should see the replies
     }
 }
